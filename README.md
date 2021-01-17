9c02fd80be8f77b57f117c650c5e69cccd2ebdf8
Sergie <sergiearruda@gmail.com>
2021-01-17 16:23:42 +0000

    Closable @support
    @supports text can now be closed

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 # Page_ideas
 
 <<<<<<< HEAD
-Current version: 1.2
+Current code version: 2.0 (under development)
 Live version on Github pages: 1.1.1
 
 Here you will be able to find web pages designed for different types of businesses and with a multitude of web technologies so you can get inspired and hopefully learn a few neat tricks.
 The CSS of the project was made with SASS (SCSS more specifically), as well as the BEM naming convention and a reduced version of the 7-1 architecture. The Javascript implemented uses some of the modern ES6 features.
 
 Updates:  
-Version 1.0 is now live
-The current version is only a basic template with the core functionalities implemented, but the existing projects are only placeholders for the future content that will be created.
+2.0 Now being developed.
+@supports text now closable
 
-Version 1.1: First actual project, "Manage Global" has been included. Minor text fixes implemented. Restructured folders copied 1.0 to a place of its own to enable version history in the site in a future implementation.
-
-1.1.1: Changed array of projects to become based on JS classes, being now more clean. Added a way to access the first project and included it to the array
+1.2: Removed placeholder projects and added personal portfolio. Expanded "more resources" section. Added text that appears if client's browser does not support some features.
 
 1.1.2: minor tag adjustment in the form in "manage global"
 
-1.2: Removed placeholder projects and added personal portfolio. Expanded "more resources" section. Added text that appears if client's browser does not support some features.
-More currently being developed...
+1.1.1: Changed array of projects to become based on JS classes, being now more clean. Added a way to access the first project and included it to the array
+
+Version 1.1: First actual project, "Manage Global" has been included. Minor text fixes implemented. Restructured folders copied 1.0 to a place of its own to enable version history in the site in a future implementation.
+
+Version 1.0 is now live
+The current version is only a basic template with the core functionalities implemented, but the existing projects are only placeholders for the future content that will be created.
--- script.js
+++ script.js
@@ -1,121 +1,142 @@
+// CLOSE SUPPORT TEXT
+const closeSupported = document.getElementById("closeSupported");
+const supportText = document.getElementById("supportText");
+closeSupported.addEventListener("click", () => {
+  supportText.style.display = "none";
+});
+
 // PAGES
 const pages = [];
 
 class Project {
-    constructor(title, description, image, url, techs) {
-        this.title = title;
-        this.description = description;
-        this.image = image;
-        this.url = url;
-        this.techs = techs;
-        Project.addInstance(this);
-    }
-    static addInstance(item) {
-        pages.push(item);
-    }
-    static getInstances() {
-        return pages;
-    }
-    static clearInstances() {
-        pages.length = 0;
-    }
+  constructor(title, description, image, url, techs) {
+    this.title = title;
+    this.description = description;
+    this.image = image;
+    this.url = url;
+    this.techs = techs;
+    Project.addInstance(this);
+  }
+  static addInstance(item) {
+    pages.push(item);
+  }
+  static getInstances() {
+    return pages;
+  }
+  static clearInstances() {
+    pages.length = 0;
+  }
 }
 
 const Project1 = new Project(
-    "Manage Global",
-    "Mockup of a fictional business brochure for an app-based company that provides business solutions",
-    "ideas/manage_global/assets/pexels-rebrand-cities-1367269.jpg",
-    "ideas/manage_global/index.html",
-    ["CSS: Bootstrap 4.5, responsivity, media queries and animations", "JavaScript: dynamic navigation bar, form validation with regular expressions, recursion, customized Bootstrap toasts and event handling for animations", "3rd party libraries: Appear on Scroll library", "Visual assets: Google Fonts, Adobe Colors, Font-Awesome icons, favicon.io, Royalty-free images (Pixabay and Pexels)", "Design tools: Photoshop for customized iPhone image"]
-)
+  "Manage Global",
+  "Mockup of a fictional business brochure for an app-based company that provides business solutions",
+  "ideas/manage_global/assets/pexels-rebrand-cities-1367269.jpg",
+  "ideas/manage_global/index.html",
+  [
+    "CSS: Bootstrap 4.5, responsivity, media queries and animations",
+    "JavaScript: dynamic navigation bar, form validation with regular expressions, recursion, customized Bootstrap toasts and event handling for animations",
+    "3rd party libraries: Appear on Scroll library",
+    "Visual assets: Google Fonts, Adobe Colors, Font-Awesome icons, favicon.io, Royalty-free images (Pixabay and Pexels)",
+    "Design tools: Photoshop for customized iPhone image",
+  ]
+);
 
 const Project2 = new Project(
-    "Personal Portfolio",
-    "My personal website and Portfolio",
-    "ideas/personal_portfolio/portfolio-sergie.png",
-    "https://sergie.net",
-    ["CSS: SASS, BEM CSS, responsivity, media queries and animations", "JavaScript: dynamic navigation bar, object-oriented programming, ES6 features", "Visual assets: Google Fonts, Adobe Colors, Font-Awesome icons, favicon.io, Royalty-free images (Pixabay and Pexels)", "Design tools: Photoshop for customized iPhone image"]
-)
+  "Personal Portfolio",
+  "My personal website and Portfolio",
+  "ideas/personal_portfolio/portfolio-sergie.png",
+  "https://sergie.net",
+  [
+    "CSS: SASS, BEM CSS, responsivity, media queries and animations",
+    "JavaScript: dynamic navigation bar, object-oriented programming, ES6 features",
+    "Visual assets: Google Fonts, Adobe Colors, Font-Awesome icons, favicon.io, Royalty-free images (Pixabay and Pexels)",
+    "Design tools: Photoshop for customized iPhone image",
+  ]
+);
 
 // GENERATE CARDS AND PREVIEW
 const cardGrid = document.getElementById("cardGrid");
 let previewTitle = document.getElementById("previewTitle");
 let previewDescription = document.getElementById("previewDescription");
 
+window.addEventListener("load", () => {
+  pages.forEach((page) => {
+    let card = document.createElement("div");
+    card.classList.add("card");
 
-window.addEventListener('load', () => {
-    pages.forEach((page) => {
-        let card = document.createElement("div");
-        card.classList.add("card");
+    let cardImage = document.createElement("div");
+    cardImage.classList.add("card__image");
+    let cardImageImg = document.createElement("img");
+    cardImageImg.setAttribute("src", page.image);
+    cardImageImg.setAttribute("alt", "image of the project");
+    cardImage.appendChild(cardImageImg);
 
-        let cardImage = document.createElement("div");
-        cardImage.classList.add("card__image");
-        let cardImageImg = document.createElement("img");
-        cardImageImg.setAttribute("src", page.image);
-        cardImageImg.setAttribute("alt", "image of the project");
-        cardImage.appendChild(cardImageImg);
+    let cardTitle = document.createElement("h3");
+    cardTitle.classList.add("card__title");
+    let cardTitleText = document.createTextNode(page.title);
+    cardTitle.appendChild(cardTitleText);
 
-        let cardTitle = document.createElement("h3");
-        cardTitle.classList.add("card__title");
-        let cardTitleText = document.createTextNode(page.title);
-        cardTitle.appendChild(cardTitleText);
+    let cardDescription = document.createElement("p");
+    cardDescription.classList.add("card__description");
+    let shortDesc = page.description;
+    if (page.description.length > 90) {
+      shortDesc = shortDesc.substr(0, 100) + "...";
+    }
+    let cardDescriptionText = document.createTextNode(shortDesc);
 
-        let cardDescription = document.createElement("p");
-        cardDescription.classList.add("card__description");
-        let shortDesc = page.description
-        if (page.description.length > 90) {
-            shortDesc = shortDesc.substr(0, 100) + "...";
-        };
-        let cardDescriptionText = document.createTextNode(shortDesc);
+    cardDescription.appendChild(cardDescriptionText);
 
-        cardDescription.appendChild(cardDescriptionText);
+    let cardButton = document.createElement("a");
+    cardButton.classList.add("card__button");
+    let cardButtonText = document.createTextNode("Access");
+    cardButton.setAttribute("href", page.url);
+    cardButton.setAttribute("target", "_blank");
+    cardButton.appendChild(cardButtonText);
 
-        let cardButton = document.createElement("a");
-        cardButton.classList.add("card__button");
-        let cardButtonText = document.createTextNode("Access");
-        cardButton.setAttribute("href", page.url);
-        cardButton.setAttribute("target", "_blank");
-        cardButton.appendChild(cardButtonText);
+    card.appendChild(cardImage);
+    card.appendChild(cardTitle);
+    card.appendChild(cardDescription);
+    card.appendChild(cardButton);
+    cardGrid.appendChild(card);
 
-        card.appendChild(cardImage);
-        card.appendChild(cardTitle);
-        card.appendChild(cardDescription);
-        card.appendChild(cardButton);
-        cardGrid.appendChild(card);
-
-        let technologies = page.techs.map(tech => "<br> " + tech)
-        card.addEventListener("mouseenter", () => {
-            previewTitle.innerHTML = page.title;
-            previewDescription.innerHTML = `${page.description}. <br> <br> This project was made using - ${technologies}.`
-        })
-    })
+    let technologies = page.techs.map((tech) => "<br> " + tech);
+    card.addEventListener("mouseenter", () => {
+      previewTitle.innerHTML = page.title;
+      previewDescription.innerHTML = `${page.description}. <br> <br> This project was made using - ${technologies}.`;
+    });
+  });
 });
 
 // SEARCH BAR
 
 // LIGHT THEME
-const lightMessages = ["Light attracts bugs", "Stop clicking here", "And you call yourself a programmer?", "Pretend everything is white", "There will never be a light theme! Maybe...", "Do you truly think this is a good idea?"];
+const lightMessages = [
+  "Light attracts bugs",
+  "Stop clicking here",
+  "And you call yourself a programmer?",
+  "Pretend everything is white",
+  "There will never be a light theme! Maybe...",
+  "Do you truly think this is a good idea?",
+];
 const lightButton = document.getElementById("lightButton");
-const lightMessage = document.getElementById("lightMessage")
+const lightMessage = document.getElementById("lightMessage");
 
-let timer = '';
+let timer = "";
 
-lightButton.addEventListener('click', () => {
-
-    clearTimeout(timer);
-    lightMessage.classList.remove("fade-in")
-    lightMessage.classList.remove("fade-out")
-    let randomMessage = lightMessages[Math.floor(Math.random() * lightMessages.length)];
-    lightMessage.classList.add("fade-in");
-    lightMessage.innerHTML = randomMessage
-    timer = setTimeout(() => {
-        // lightMessage.classList.remove("fade-ind");
-        lightMessage.classList.add("fade-out");
-    }, 2000)
-
+lightButton.addEventListener("click", () => {
+  clearTimeout(timer);
+  lightMessage.classList.remove("fade-in");
+  lightMessage.classList.remove("fade-out");
+  let randomMessage =
+    lightMessages[Math.floor(Math.random() * lightMessages.length)];
+  lightMessage.classList.add("fade-in");
+  lightMessage.innerHTML = randomMessage;
+  timer = setTimeout(() => {
+    // lightMessage.classList.remove("fade-ind");
+    lightMessage.classList.add("fade-out");
+  }, 2000);
 });
-
-
 
 // GET YEAR DINAMICALLY
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
@@ -126,16 +147,16 @@ const aboutButton = document.getElementById("aboutButton");
 const pagesSection = document.getElementById("pagesSection");
 const aboutSection = document.getElementById("aboutSection");
 
-pagesButton.addEventListener('click', () => {
-    pagesButton.classList.add("btn--active");
-    aboutButton.classList.remove("btn--active");
-    pagesSection.classList.add("active-section");
-    aboutSection.classList.remove("active-section");
-})
+pagesButton.addEventListener("click", () => {
+  pagesButton.classList.add("btn--active");
+  aboutButton.classList.remove("btn--active");
+  pagesSection.classList.add("active-section");
+  aboutSection.classList.remove("active-section");
+});
 
-aboutButton.addEventListener('click', () => {
-    pagesButton.classList.remove("btn--active");
-    aboutButton.classList.add("btn--active");
-    pagesSection.classList.remove("active-section");
-    aboutSection.classList.add("active-section");
-})
+aboutButton.addEventListener("click", () => {
+  pagesButton.classList.remove("btn--active");
+  aboutButton.classList.add("btn--active");
+  pagesSection.classList.remove("active-section");
+  aboutSection.classList.add("active-section");
+});
